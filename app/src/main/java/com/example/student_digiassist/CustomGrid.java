98b324f724aa6af5ceb5dc47cd5a539e9f301992
Anirudhpolawar <agpolawar1@gmail.com>
2020-04-24 15:42:04 +0530

    Added the GUI of the notes Screen (notes.java grid.xml customgrid.java shape.xml grid.xml)

---
+++ app/src/main/java/com/example/student_digiassist/CustomGrid.java
@@ -0,0 +1,62 @@
+package com.example.student_digiassist;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomGrid extends BaseAdapter{
+    private Context mContext;
+    private final String[] title;
+
+
+    public CustomGrid(Context c,String[] title) {
+        mContext = c;
+
+        this.title = title;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return title.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            grid = new View(mContext);
+            grid = inflater.inflate(R.layout.grid, null);
+            TextView textView = (TextView) grid.findViewById(R.id.grid_text);
+            textView.setText(title[position]);
+            textView.setBackgroundResource(R.drawable.shape);
+
+        } else {
+            grid = (View) convertView;
+        }
+
+        return grid;
+    }
+}
