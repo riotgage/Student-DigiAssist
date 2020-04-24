91bde4eaf0c15c813c22c8b437fd734b6696d024
riotgage <rvsatwadhar@gmail.com>
2020-04-24 16:34:26 +0530

    Alerts Page completed, All data is pre fed...Webdata yet to be attached

--- app/src/main/java/com/example/student_digiassist/MainActivity.java
+++ app/src/main/java/com/example/student_digiassist/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.student_digiassist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.student_digiassist.competition.Alerts;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView alerts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alerts=findViewById(R.id.alerts);
+
+        alerts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this, Alerts.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
